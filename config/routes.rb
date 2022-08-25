8bb07100b0102d8256673ebe8f0abddf964d37cd
EC2 Default User <wakana980830@gmail.com>
2022-08-25 07:33:32 +0000

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get '/'=>"homes#top"
+    get '/about'=>"homes#about"
+  end
+  namespace :admin do
+    root to: "homes#top"
+  end
 # 管理者用
 # URL /admin/sign_in ...
   devise_for :admin,skip:[:registrations,:passwords],controllers:{
     sessions:"admin/sessions"
   }
+  
+  namespace:admin do
+    resources:items
+    resources:genres
+    resources:customers
+    resources:cart_items
+    resources:orders
+    resources:order_details
+  end
   
  # 顧客用
 # URL /customers/sign_in ...
@@ -11,5 +27,14 @@ Rails.application.routes.draw do
     registrations:"public/registrations",
     sessions:'public/sessions'
   }
+  
+  namespace:public do
+    resources:items
+    resources:homes
+    resources:customers
+    resources:cart_items
+    resources:orders
+    resources:order_details
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
