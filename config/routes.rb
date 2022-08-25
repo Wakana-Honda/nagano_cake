1b05f251735fe95c11399385a6431a83388db677
EC2 Default User <wakana980830@gmail.com>
2022-08-25 12:40:03 +0000

    ログイン画面頑張れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,30 @@
 Rails.application.routes.draw do
+
+# 管理者用
+# URL /admin/sign_in ...
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers:{
+    sessions:"admin/sessions"
+  }
+# 顧客用
+# URL /customers/sign_in ...
+ devise_for :customers,skip:[:passwords],controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+  
   namespace :public do
     get '/'=>"homes#top"
     get '/about'=>"homes#about"
-  end
-  namespace :admin do
-    root to: "homes#top"
-  end
-# 管理者用
-# URL /admin/sign_in ...
-  devise_for :admin,skip:[:registrations,:passwords],controllers:{
-    sessions:"admin/sessions"
-  }
-  
-  namespace:admin do
     resources:items
-    resources:genres
     resources:customers
     resources:cart_items
     resources:orders
     resources:order_details
   end
-  
- # 顧客用
-# URL /customers/sign_in ...
-  devise_for :customers,skip:[:passwords],controllers:{
-    registrations:"public/registrations",
-    sessions:'public/sessions'
-  }
-  
-  namespace:public do
+  namespace :admin do
+    root to: "homes#top"
     resources:items
-    resources:homes
+    resources:genres
     resources:customers
     resources:cart_items
     resources:orders
