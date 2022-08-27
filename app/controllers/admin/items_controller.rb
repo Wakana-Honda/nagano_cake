20e4cc66b6a2b8e3589d8c9c25963e9a2592d812
EC2 Default User <wakana980830@gmail.com>
2022-08-27 10:12:16 +0000

    bootsstarap導入してこう

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,9 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+  end
+  
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 # 管理者用
 # URL /admin/sign_in ...
  devise_for :admin, skip: [:registrations, :passwords] ,controllers:{
-    sessions:"admin/sessions"
+   sessions:"admin/sessions"
   }
+  
 # 顧客用
 # URL /customers/sign_in ...
  devise_for :customers,skip:[:passwords],controllers:{
@@ -12,23 +13,29 @@ Rails.application.routes.draw do
     sessions:'public/sessions'
   }
   
-  namespace :public do
-    get '/'=>"homes#top"
+  scope module: :public do
+   root to:'homes#top'
     get '/about'=>"homes#about"
-    resources:items
-    resources:customers
-    resources:cart_items
-    resources:orders
-    resources:order_details
+    resources:items,only:[:index,:show]
+    resources:customers,only:[:show,:edit,:update]
+    get 'customers/unsbscribe'=>"customers#subscribe"
+    patch 'customers/withdraw'=>"customers#withdraw"
+    resources:cart_items,only:[:index,:update,:destroy,:create]
+    delete 'cart_items/destory_all'=>"cart_items#destory_all"
+    resources:orders,only:[:new,:create,:index,:show]
+    post 'orders/confirm'=>"orders#confirm"
+    get 'orders/complete'=>"orders#complete"
+    #thanks letter
+    resources:adresses,only:[:index,:edit,:create,:update,:destroy]
   end
+  
   namespace :admin do
-    root to: "homes#top"
-    resources:items
-    resources:genres
-    resources:customers
-    resources:cart_items
-    resources:orders
-    resources:order_details
+    get '/'=>"homes#top"
+    resources:items,only:[:index,:new,:create,:show,:edit,:update]
+    resources:genres,only:[:index,:create,:edit,:update]
+    resources:customers,only:[:index,:show,:edit,:update]
+    resources:orders,only:[:show,:update]
+    resources:order_details,only:[:update]   
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
