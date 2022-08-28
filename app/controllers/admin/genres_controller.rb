d1898047e473a2c263898fd375a41818cc4f9c1a
EC2 Default User <wakana980830@gmail.com>
2022-08-28 15:57:59 +0000

    ログインできるようにジャンルモデル聞いてみて

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,21 @@
 class Admin::GenresController < ApplicationController
-  def index
-  end
+  
+ def index
+   @genre=Genre.new
+ end
+ 
+ def create
+   @genre.save
+ end
  
  def edit
+   
  end
 
+private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_022249) do
+ActiveRecord::Schema.define(version: 2022_08_28_143715) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 2022_08_25_022249) do
     t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
