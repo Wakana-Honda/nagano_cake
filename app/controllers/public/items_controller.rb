acc2b71f598dc23d4c2a59c0748147d6f0d35939
EC2 Default User <wakana980830@gmail.com>
2022-09-06 12:22:51 +0000

    今日は疲れました

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
-    @customer = current_customer
+   @item = Item.all
+   @items = Item.page(params[:page]).per(2)
+   @genres = Genre.all
+  end
+  
+  def show
+   @item = Item.find(params[:id])
+   @genres = Genre.all
   end
 end
