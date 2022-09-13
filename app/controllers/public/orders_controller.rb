ab0ecae726de10abb56d49b952c6d71173c694e7
EC2 Default User <wakana980830@gmail.com>
2022-09-13 04:41:30 +0000

    頑張ろう

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,21 @@
 class Public::OrdersController < ApplicationController
+  def new
+   @customer = current_customer
+  end
+  
+  def comfirm
+  end
+  
+  def create
+  end
+  
+  def complete
+  end
+  
+  def index 
+  end
+  
+  def show
+  end
+
 end
