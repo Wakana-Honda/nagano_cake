29ea1fac91ee8a1ea85196e16a1b0659bf1bd400
EC2 Default User <wakana980830@gmail.com>
2022-09-19 07:23:34 +0000

    送料とかやってね

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,6 @@ class Public::OrdersController < ApplicationController
   def new
    @adresses = Adress.all
    @order = Order.new
-   # @total = 0
    @customer = current_customer
   end
   
@@ -10,9 +9,10 @@ class Public::OrdersController < ApplicationController
    @order = Order.new(order_params)
    # @order.payment_method = params[:payment_method]
    @cart_items = current_customer.cart_items
+   # @order.shopping_cost = 800
    @total = 0
-   # select_adress = params[:order][:select_adress]
-    # binding.pry
+   # @order.total_payment = @order.shopping_cost + @total
+     # binding.pry
    if params[:order][:select_adress] == '0'
      @order = Order.new(order_params)
      @order.postal_code = current_customer.postal_code
@@ -20,7 +20,7 @@ class Public::OrdersController < ApplicationController
      @order.name = current_customer.first_name + current_customer.last_name
    
    elsif params[:order][:select_adress] == '1'
-     @adress = Adress.find(params[:order][:select_adress])
+     @adress=Adress.find(params[:order][:adress_id])
      @order.postal_code = @adress.postal_code
      @order.adress = @adress.adress
      @order.name = @adress.name
@@ -39,10 +39,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-   # @customer = current_customer
     @order = Order.new(order_params)
     @order.save
-    redirect_to confirm_orders_path
+     
+    redirect_to complete_orders_path
    
   end
   
@@ -50,6 +50,7 @@ class Public::OrdersController < ApplicationController
   end
   
   def index 
+   @orders = Order.all
   end
   
   def show
