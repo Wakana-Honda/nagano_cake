af3dd9646cbca4103a915dfa4e4685a269169e38
EC2 Default User <wakana980830@gmail.com>
2022-09-27 01:44:21 +0000

    退会アラーム

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,7 +18,7 @@ class Public::CustomersController < ApplicationController
  
  def unsubscribe
   @customer = current_customer
-  # @customer = Customer.find_by(email:params[:customer][:email])
+  # @customer = Customer.find_by(email:params[:customer]s[:email])
  end
  
  def withdraw
