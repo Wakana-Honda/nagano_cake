f3398fe646f34550df0a82093ce254fd4f4afb4f
EC2 Default User <wakana980830@gmail.com>
2022-09-29 10:20:54 +0000

    動画作成頑張って

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,11 +2,11 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(_resource)
-    customers_my_page_path(current_customer)
+    customers_my_page_path
   end
   # GET /resource/sign_up
   # def new
